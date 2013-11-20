6ac52f58bb98328c57bffef787489af2c14db13f
Gu1 <gu1@cafai.fr>
2013-11-20 05:56:07 +0100

    Use gevent in run.py, as it's the case in prod

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
+import gevent.pywsgi
+from gevent import monkey; monkey.patch_all()
+import werkzeug.serving
+from werkzeug.debug import DebuggedApplication
 from ffdnispdb import app
-app.run(debug=True, threaded=True)
+
+
+@werkzeug.serving.run_with_reloader
+def runServer():
+    app.debug = True
+    ws = gevent.pywsgi.WSGIServer(('', 5000), DebuggedApplication(app, evalex=True))
+    ws.serve_forever()
+
